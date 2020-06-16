0432c871cfc665ed73c407f0e29d21b1251ef44d
Kassian Rosner Wren <the@nodebotani.st>
2020-06-16 23:22:03 +0100

    lamp working

--- pi_code/index.js
+++ pi_code/index.js
@@ -1,11 +1,31 @@
 const Raspi = require('raspi-io').RaspiIO
 const five = require('johnny-five')
+const restify = require('restify')
 
 const board = new five.Board({
-	io: new Raspi()
+  io: new Raspi()
 })
 
+const server = restify.createServer()
+
 board.on('ready', () => {
-	const lamp = new five.Led('P1-7')
-	lamp.strobe();
+  let lamp = new five.Led('GPIO6')
+
+  server.get('/lamp/:on', setLampState)
+
+  function setLampState(req, res, next) {
+    let state = req.params.on === 'true'
+    if(state){
+      lamp.on()
+      res.send('lamp on')
+    } else {
+      lamp.off()
+      res.send('lamp off')
+    }
+    next()
+  }
+
+  server.listen(8000, () => {
+    console.log('server listening on port ' + server.url)
+  })
 })
