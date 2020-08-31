e045d3a6eddf8378f23111c479cd23569e96b692
nodebotanist <the@nodebotani.st>
2020-08-31 12:59:34 -0500

    Added iot dashboard worker

--- worker_code/iot-dashboard/public/app.js
+++ worker_code/iot-dashboard/public/app.js
@@ -1,21 +1,5 @@
 (async function(){
-	let ctx = document.getElementById('temp-graph').getContext('2d')
-	// var chart = new Chart(ctx, {
-	// 	type: 'line',
-	// 	data: [],
-	// 	options: {
-	// 		scales: {
-	// 			xAxes: [{
-	// 				type: 'time',
-	// 				time: {
-	// 					displayFormats: {
-	// 						quarter: 'MMM YYYY'
-	// 					}
-	// 				}
-	// 			}]
-	// 		}
-	// 	}
-	// })
+
 
 	let data = await fetch('https://show-temp.nodebotanist.workers.dev/', {
 		method: 'GET',
@@ -24,6 +8,65 @@
 		jsonData = await data.json()
 		jsonData = JSON.parse(jsonData)
 		jsonData = jsonData.slice(jsonData.length - 50, jsonData.length)
-		console.log(jsonData)
+		temperatureData = []
+		humidityData = []
+		jsonData.forEach((item, index) => {
+			temperatureData.push({
+				x: index,
+				y: item.temp
+			})
+			humidityData.push({
+				x: item.time,
+				y: item.relativeHumidity
+			})
+		})
+
+		console.log(temperatureData, humidityData)
+
+		let ctx = document.getElementById('temp-graph').getContext('2d')
+		var chart = new Chart(ctx, {
+			type: 'line',
+			data: temperatureData,
+			options: {
+				scales: {
+					xAxes: [{
+						type: 'time',
+						time: {
+							// parser: 'YYYY-MM-DD HH:mm:ss.SSS',
+						},
+						bounds: 'data'
+					}],
+					yAxes: [{
+						type: 'linear',
+						ticks: {
+							min: 0,
+							max: 40
+						}
+					}]
+				}
+			}
+		})
+	})
+	let ctx2 = document.getElementById('humidity-graph').getContext('2d')
+	var humidityChart = new Chart(ctx2, {
+		type: 'line',
+		data: humidityData,
+		options: {
+			scales: {
+				xAxes: [{
+					type: 'time',
+					time: {
+						// parser: 'YYYY-MM-DD HH:mm:ss.SSS',
+					}
+				}],
+				yAxes: [{
+					type: 'linear',
+					ticks: {
+						min: 0,
+						max: 90
+					}
+				}]
+			}
+		}
 	})
 })()
