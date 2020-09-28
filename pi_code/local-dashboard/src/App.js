ee872d4d058720e9139e52e258c6e4450f17ba6f
Kassian Rosner Wren <the@nodebotani.st>
2020-09-28 19:36:27 +0100

    Added start of local dashboard

---
+++ pi_code/local-dashboard/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import NavBar from './components/navbar'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+    </div>
+  );
+}
+  
+export default App;
---
+++ pi_code/local-dashboard/src/components/navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function NavBar() {
+	return (
+		<nav class="navbar navbar-light bg-light">
+			<a class="navbar-brand" href="#">
+				Bootstrap
+			</a>
+		</nav>)
+}
+
+export default NavBar
