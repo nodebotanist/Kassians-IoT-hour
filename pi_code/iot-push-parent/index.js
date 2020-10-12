87f673f4a14c19f2dc0667d3b266db764b3f8d43
Kassian Rosner Wren <the@nodebotani.st>
2020-10-12 19:37:11 +0100

    Added hapi

--- pi_code/iot-push-parent/index.js
+++ pi_code/iot-push-parent/index.js
@@ -1,58 +1,85 @@
 const http = require("http")
-const Router = require('router')
 const redis = require("redis")
-const { Console } = require("console")
+const Hapi = require('@hapi/hapi')
+const Bluebird = require('bluebird')
+
+const pubsub = redis.createClient()
+const tempSubscriber = redis.createClient()
+const promiselrange = Bluebird.promisify(pubsub.lrange, {context: pubsub})
 
 const host = 'localhost'
 const port = 8000
 
-const router = Router()
+const init = async () => {
 
-const pubsub = redis.createClient()
-const tempSubscriber = redis.createClient()
-
-router.get('/', (req, res) => {
-		pubsub.publish("iot-push", 'main')
-		res.setHeader("Content-Type", "application/json")
-		res.writeHead(200)
-		res.end(`{"message": "Message published"}`)
-})
-
-router.get('/sensor-data/:type', (req, res) => {
-	res.setHeader("Content-Type", "application/json")
-	res.writeHead(200)
-	res.end(`{"type": "${type}"}`)
-})
-
-router.get('/child-access', (req, res) => {
-	console.log('access')
-	pubsub.publish('iot-push', new Date().toString())
-	res.writeHead(200)
-	res.setHeader('Content-Type', 'application/json')
-	res.end(`{"message": "child access logged!"}`)
-})
-
-router.post('/temp-data', (req, res)=>{
-	let body = '';
-	req.on('data', chunk => {
-			body += chunk.toString(); // convert Buffer to string
+	const server = Hapi.server({
+		port,
+		host
 	});
-	req.on('end', () => {
-		body = JSON.parse(body)
-		console.log(body)
-		if(body && body.temp){
-			pubsub.publish('temp-data', body.temp)
-			res.setHeader('Content-Type', 'application/json')
-			res.writeHead(200)
-			res.end(`{"message": "Temperature data sent"}`)
-		} else {
-			res.setHeader('Content-Type', 'application/json')
-			res.writeHead(400)
-			res.end(`{"error": "Need a temp field in a JSON body"}`)
+
+	server.route({
+		method: ['GET'],
+		path: '/', 
+		handler: (req, h) => {
+			pubsub.publish("iot-push", 'main')
+			return `{"message": "Message published"}`
 		}
-	});
+	})
 
-})
+	server.route({
+		method: ['GET'],
+		path: '/sensor-data/{zone}/{type}', 
+		handler: async (req, h) => {
+			const result = await new Bluebird((resolve) => {
+				promiselrange(`${req.params.zone}-${req.params.type}`, 0, 25)
+					.then((result) => {
+						resolve(result)
+					})
+			})
+			return result
+		}
+	})
+
+	await server.start()
+	console.log('Server running on %s', server.info.uri)
+};
+
+process.on('unhandledRejection', (err) => {
+	console.log(err)
+	process.exit(1)
+});
+
+
+
+// router.get('/child-access/', (req, res) => {
+// 	console.log('access')
+// 	//pubsub.publish('iot-push', new Date().toString())
+// 	res.writeHead(200)
+// 	res.setHeader('Content-Type', 'application/json')
+// 	res.end(`{"message": "child access logged!"}`)
+// })
+
+// router.post('/temp-data/', (req, res)=>{
+// 	let body = '';
+// 	req.on('data', chunk => {
+// 			body += chunk.toString(); // convert Buffer to string
+// 	});
+// 	req.on('end', () => {
+// 		body = JSON.parse(body)
+// 		console.log(body)
+// 		if(body && body.temp){
+// 			pubsub.publish('temp-data', body.temp)
+// 			res.setHeader('Content-Type', 'application/json')
+// 			res.writeHead(200)
+// 			res.end(`{"message": "Temperature data sent"}`)
+// 		} else {
+// 			res.setHeader('Content-Type', 'application/json')
+// 			res.writeHead(400)
+// 			res.end(`{"error": "Need a temp field in a JSON body"}`)
+// 		}
+// 	});
+
+// })
 
 tempSubscriber.on("message", (channel, message) => {
 	console.log(`Channel: ${channel} Message: ${message}`)
@@ -73,11 +100,5 @@ tempSubscriber.subscribe('iot-push')
 tempSubscriber.subscribe('temp-data')
 tempSubscriber.subscribe('iot-sensor-data')
 
-const requestListener = function (req, res) {
-	router(req, res, () => { })
-}
 
-const server = http.createServer(requestListener)
-server.listen(port, host, () => {
-	console.log(`Server is running on http://${host}:${port}`)
-})
+init()
